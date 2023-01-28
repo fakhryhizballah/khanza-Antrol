37db7d1cbfac8e38c3abb51bba3962f08b5eaca2
fakhryhizballah <falehry88@gmail.com>
2023-01-28 16:09:22 +0700

    midell ware

--- routes/icd.js
+++ routes/icd.js
@@ -5,7 +5,7 @@ const middleware = require('../middleware');
 
 const icd = require('../controllers/icd');
 
-routes.get('/10',middleware.check,middleware.checkDokter, icd.geticd10);
-routes.get('/9', icd.geticd9);
+routes.get('/10',middleware.check, icd.geticd10);
+routes.get('/9', middleware.checkDokter,icd.geticd9);
 
 module.exports = routes;
