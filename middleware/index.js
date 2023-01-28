86b3fce3a6b1af7ea1899ac1f611c386f20a321e
fakhryhizballah <falehry88@gmail.com>
2023-01-28 15:53:05 +0700

    ICD with middel were

---
+++ middleware/index.js
@@ -0,0 +1,62 @@
+const jwt = require('jsonwebtoken');
+module.exports ={
+    check : async (req, res, next) => {
+        try {
+            const token = (req.headers['authorization']).split(' ')[1];
+            jwt.verify(token, process.env.JWT_SECRET_KEY);
+            next();
+        } catch (err) {
+            return res.status(401).json({
+                status: false,
+                message: err.message,
+                data: null
+            });
+        }
+    },
+    checkAdmin : async (req, res, next) => {
+        try {
+            const token = (req.headers['authorization']).split(' ')[1];
+            const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+            let obj = decoded.kd_access.find(o => o.lvid === 1);
+            console.log(obj);
+            if (obj) {
+                next();
+            } else {
+                return res.status(401).json({
+                    status: false,
+                    message: 'You are not admin',
+                    data: null
+                });
+            }
+        } catch (err) {
+            return res.status(401).json({
+                status: false,
+                message: err.message,
+                data: null
+            });
+        }
+    },
+    checkDokter : async (req, res, next) => {
+        try {
+            const token = (req.headers['authorization']).split(' ')[1];
+            const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+            let obj = decoded.kd_access.find(o => o.lvid === 3);
+            console.log(obj);
+            if (obj) {
+                next();
+            } else {
+                return res.status(401).json({
+                    status: false,
+                    message: 'You are not dokter',
+                    data: null
+                });
+            }
+        } catch (err) {
+            return res.status(401).json({
+                status: false,
+                message: err.message,
+                data: null
+            });
+        }
+    }
+}
--- routes/icd.js
+++ routes/icd.js
@@ -1,10 +1,11 @@
 'use strict';
 const express = require("express");
 const routes = express.Router();
+const middleware = require('../middleware');
 
 const icd = require('../controllers/icd');
 
-routes.get('/10', icd.geticd10);
+routes.get('/10',middleware.check,middleware.checkDokter, icd.geticd10);
 routes.get('/9', icd.geticd9);
 
 module.exports = routes;
