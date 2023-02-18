a2b2e4093442553988edab32d79f76a7853850f7
fakhryhizballah <falehry88@gmail.com>
2023-02-18 09:33:57 +0700

    middelware ralan

--- routes/ralan.js
+++ routes/ralan.js
@@ -2,12 +2,13 @@
 const { Route } = require("express");
 const express = require("express");
 const routes = express.Router();
+const middleware = require('../middleware');
 
 const ralan = require('../controllers/ralan');
 
 
-routes.get('/igd', ralan.getIGD);
-routes.get('/poli', ralan.getPoli);
-routes.get('/poli/:kd_poli', ralan.getPoliByKdPoli);
+routes.get('/igd',middleware.check, ralan.getIGD);
+routes.get('/poli',middleware.check, ralan.getPoli);
+routes.get('/poli/:kd_poli',middleware.check, ralan.getPoliByKdPoli);
 
 module.exports = routes;
