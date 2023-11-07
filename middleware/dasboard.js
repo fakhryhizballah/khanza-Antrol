b5ebb90b4f03ccf4c59e1ea5323bb0275e7a8ba1
fakhryhizballah <falehry88@gmail.com>
2023-11-07 11:25:01 +0700

    dasboard ralan

---
+++ middleware/dasboard.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+module.exports ={
+    dashboard : async (req, res, next) => {
+        try {
+            const token = (req.headers['authorization']).split(' ')[1];
+            let data =   jwt.verify(token, process.env.JWT_SECRET_KEY);
+            console.log(data.permission);
+            if(data.permission.includes('dashboard')){
+                next();
+            }else{
+                return res.status(401).json({
+                    status: false,
+                    message: 'Unauthorized Access Dashboard',
+                    data: null
+                });
+            }
+           
+        } catch (err) {
+            return res.status(401).json({
+                status: false,
+                message: err.message,
+                data: null
+            });
+        }
+    },
+}
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,11 +1,12 @@
 'use strict';
 const express = require("express");
 const routes = express.Router();
-const middleware = require('../middleware');
+const middleware = require('../middleware/dasboard');
 
 const dashboard = require('../controllers/dashboard');
 
 // routes.get('/regpoli/daily', middleware.check, dashboard.poliHarian);
-routes.get('/regpoli/day', middleware.check, dashboard.poliHarian);
+routes.get('/regpoli/day', middleware.dashboard, dashboard.poliHarian);
+routes.get('/regpoli/reports', middleware.dashboard, dashboard.getKunjungan);
 
 module.exports = routes;
