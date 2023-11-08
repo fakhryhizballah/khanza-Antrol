6884036f6d3f3463cf29e5f975ac8f62af0deb2c
fakhryhizballah <falehry88@gmail.com>
2023-11-08 11:04:25 +0700

    get asurasi

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -169,6 +169,23 @@ module.exports = {
       });
     }
   },
+  getAsuransi: async (req, res) => {
+    try {
+      const param = req.query;
+      return res.status(200).json({
+        status: true,
+        message: "poliHarian",
+        record: 0,
+        data: param,
+      });
+    } catch (err) {
+      return res.status(400).json({
+        status: false,
+        message: "Bad Request",
+        data: err,
+      });
+    }
+  },
   getpenyakit: async (req, res) => {
     try {
       const param = req.query;
