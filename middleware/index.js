6dffe1e935a228762ef1ebea6e890732f071e9f2
fakhryhizballah <falehry88@gmail.com>
2023-11-21 09:36:24 +0700

    authorization validasi

--- middleware/index.js
+++ middleware/index.js
@@ -2,6 +2,13 @@ const jwt = require('jsonwebtoken');
 module.exports ={
     check : async (req, res, next) => {
         try {
+            if (!req.headers['authorization']){
+                return res.status(401).json({
+                    status: false,
+                    message: 'Unauthorized',
+                    data: null
+                });
+            }
             const token = (req.headers['authorization']).split(' ')[1];
             jwt.verify(token, process.env.JWT_SECRET_KEY);
             next();
