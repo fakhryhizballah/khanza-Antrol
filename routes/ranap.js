60f12f87d53e4b1be2dade30d87ddf542abf3e79
fakhryhizballah <falehry88@gmail.com>
2023-12-05 22:17:03 +0700

    Add dashboard route for getting patients who haven't been discharged

--- routes/ranap.js
+++ routes/ranap.js
@@ -3,11 +3,13 @@ const express = require("express");
 const routes = express.Router();
 const middleware = require('../middleware');
 const ranap = require('../controllers/ranap');
+const dashboard = require('../controllers/dashboard');
 
 
 routes.get('/bp',middleware.check, ranap.getBp);
 routes.get('/tm',middleware.check, ranap.getTglMasuk);
 routes.get('/kamar',middleware.check, ranap.getKamar);
 routes.get('/belumpulang', middleware.check, ranap.getBelumPulang);
+routes.get('/dasboard/belumpulang', middleware.check, dashboard.getBelumPulang);
 
 module.exports = routes;
