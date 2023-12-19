72d12b95fda63c38979d824c9eb97cdfa3c966a8
fakhryhizballah <falehry88@gmail.com>
2023-12-19 20:17:17 +0700

    add middelware

--- routes/registrasi.js
+++ routes/registrasi.js
@@ -4,12 +4,10 @@ const routes = express.Router();
 const middleware = require('../middleware');
 const registrasi = require('../controllers/registrasi');
 
-
-// routes.post('/bookingperiksa', middleware.check, registrasi.bookingPeriksa);
-routes.post('/bookingperiksa', registrasi.bookingPeriksa);
-routes.get('/bookingperiksa', registrasi.cekBookingPeriksa);
-routes.post('/bookingperiksa/batal', registrasi.batalBookingPeriksa);
-routes.post('/bookingperiksa/cekin', registrasi.cekinBookingPeriksa);
+routes.post('/bookingperiksa', middleware.check, registrasi.bookingPeriksa);
+routes.get('/bookingperiksa', middleware.check, registrasi.cekBookingPeriksa);
+routes.post('/bookingperiksa/batal', middleware.check, registrasi.batalBookingPeriksa);
+routes.post('/bookingperiksa/cekin', middleware.check, registrasi.cekinBookingPeriksa);
 
 
 module.exports = routes;
